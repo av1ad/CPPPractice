338ce6ca3edf2016547846e7a1a488ddc20ab5ae
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-11-20 17:19:00 -0500

    added static calculator

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+
+    float firstNumber = 11.0124;
+    float secondNumber = 10;
+    float sum = firstNumber + secondNumber;
+
+    std::cout << firstNumber << '\n';
+    std::cout << secondNumber  << '\n';
+    std::cout << "Sum of " << firstNumber << " + " << secondNumber << " is " << sum << '\n';
+    return 0;
+}
