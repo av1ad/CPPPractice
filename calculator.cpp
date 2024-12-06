0b97e06d42aedbee18a7582e2e21ad9949daf90c
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-06 14:48:00 -0500

    I feel like there is an easier way to do it but this will work for now

--- calculator.cpp
+++ calculator.cpp
@@ -5,9 +5,10 @@
 int main()
 {
 
-    double firstNumber;
-    double secondNumber;
+    long firstNumber;
+    long secondNumber;
     std::string operation;
+    std::string sum;
 
     std::cout << "Enter first number: ";
     std::cin >> firstNumber;
@@ -15,6 +16,21 @@ int main()
     std::cin >> operation;
     std::cout << "Enter second number: ";
     std::cin >> secondNumber;
-    std::cout << "The sum of " << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << std::endl;
+
+    if(operation == "+") {
+        std::cout << "The sum of " << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << std::endl;
+    } else if (operation == "-") {
+        std::cout << "The sum of " << firstNumber << " - " << secondNumber << " is " << firstNumber - secondNumber << std::endl;
+    } else if (operation == "/") {
+        if(firstNumber == 0 || secondNumber == 0) {
+            std::cout << "You cannot divide by 0";
+        } else {
+            std::cout << "The sum of " << firstNumber << " / " << secondNumber << " is " << firstNumber / secondNumber << std::endl;
+        }
+    } else if (operation == "*") {
+        std::cout << "The sum of " << firstNumber << " * " << secondNumber << " is " << firstNumber * secondNumber << std::endl;
+    } else {
+        std::cout << "This is not a valid operation, please input a valid operation next time.";
+    }
     return 0;
 }
