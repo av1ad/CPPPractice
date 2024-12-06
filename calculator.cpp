224fe2208784d17d26f73835e07fe6894b4e819b
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-06 14:34:37 -0500

    Trying to make a simple calculator

--- calculator.cpp
+++ calculator.cpp
@@ -7,9 +7,12 @@ int main()
 
     double firstNumber;
     double secondNumber;
+    std::string operation;
 
     std::cout << "Enter first number: ";
     std::cin >> firstNumber;
+    std::cout << "Enter an operation (+, -, /, *): ";
+    std::cin >> operation;
     std::cout << "Enter second number: ";
     std::cin >> secondNumber;
     std::cout << "The sum of " << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << std::endl;
